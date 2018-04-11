aa34f23e3ff5a3fafc76f0674674f9c2a9c26c27
shmysteel <shmysteel@shmysteeldeMacBook-Pro.local>
2018-04-11 19:18:35 +0800

    11

--- gitLearn/gitLearn/HomeViewController.m
+++ gitLearn/gitLearn/HomeViewController.m
@@ -18,7 +18,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"主页";
+    
 }
+
+/**
+ 注册
+ */
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
