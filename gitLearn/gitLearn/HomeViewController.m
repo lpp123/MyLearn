7046583b35abe1246a7651b4032b19ff4bba1d2d
shmysteel <shmysteel@shmysteeldeMacBook-Pro.local>
2018-04-11 19:26:34 +0800

    1111

--- gitLearn/gitLearn/HomeViewController.m
+++ gitLearn/gitLearn/HomeViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"主页";
+    self.navigationController.navigationBar.translucent = NO;
     
 }
 
